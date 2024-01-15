2e135ae6af7d4582d57c1b808f7ca505e356866e
Nivastej <63730345+Nivastej@users.noreply.github.com>
2024-01-15 02:59:15 +0000

    done

--- main.py
+++ main.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/api')
+def api():
+    return render_template('app.html')
+    
+ 
+  
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
